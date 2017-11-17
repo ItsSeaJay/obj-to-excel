ad50e75459364863f8549210a3ff15aafd71680d
Unknown <b6010572@my.shu.ac.uk>
2017-11-17 19:35:39 +0000

    Started Learning the Visual Studio Command Line

--- source/main.cpp
+++ source/main.cpp
@@ -1,8 +1,22 @@
 #include <iostream>
+#include <string>
+#include <vector>
 
 int main(int argc, char const *argv[])
 {
-  cout << "Hello, World!\n";
+
+
+  if (argc > 1)
+  {
+    for (int i = 0; i < argc; ++i)
+    {
+      std::cout << argv[i];
+    }
+  }
+  else
+  {
+    std::cout << "Bar";
+  }
 
   return 0;
 }
